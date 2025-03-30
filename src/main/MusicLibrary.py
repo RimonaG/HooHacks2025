7aea1140a861e1dbb37a0920bacc3cd6778d5daa
carol <carolyncinbox@gmail.com>
2025-03-29 23:48:17 -0400

    testing

--- src/main/MusicLibrary.py
+++ src/main/MusicLibrary.py
@@ -1,7 +1,12 @@
 
 class MusicLibrary:
-    musicLibrary = {"Happy": ["song1", "song2"], "Sad": ["song3", "song4"],
+    music_Library = {"Happy": ["song1", "song2"], "Sad": ["song3", "song4"],
                 "Angry" : ["song5", "song6"]} #+more
+
+    # Constructor method - needs 'self' as parameter
+    def __init__(self):
+        if self not in music_Library:
+            music_Library[self] = []
 
 
     def get(str):
