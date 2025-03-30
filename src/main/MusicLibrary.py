0db6c13fe2681f52faf8793d73262fc9b77a84e0
RimonaG. <cna8eg@virginia.edu>
2025-03-30 07:36:02 -0400

    Update library and make gui open link

--- src/main/MusicLibrary.py
+++ src/main/MusicLibrary.py
@@ -3,8 +3,10 @@ import webbrowser
 #from tkinter import messagebox
 
 class MusicLibrary:
-    music_library = {"happy": ["https://youtu.be/ZbZSe6N_BXs?si=Vv8UsoWLnXugD10C", "https://youtu.be/5NV6Rdv1a3I?si=z6yo9eb0AaaC5Ceh"], "sad": ["https://youtu.be/pB-5XG-DbAA?si=6KbkkYTIasxiu7gB", "https://youtu.be/k4V3Mo61fJM?si=iqPJN6SGt6DYawiT"],
-                "angry": ["https://youtu.be/CdhqVtpR2ts?si=ppEAZVi8xB2MDvia", "https://youtu.be/OBqw818mQ1E?si=jkF3crJLPb6ZjiDO"], "surprised": ["https://youtu.be/u5CVsCnxyXg?si=8jV3G07Dw8Og7HcN"]} #+more
+    music_library = {"happy": ["https://www.youtube.com/watch?v=zoq0tAKpLBI&ab_channel=TheLivingTombstone-Topic", "https://youtu.be/ZbZSe6N_BXs?si=Vv8UsoWLnXugD10C", "https://youtu.be/5NV6Rdv1a3I?si=z6yo9eb0AaaC5Ceh"],
+                     "sad": ["https://www.youtube.com/watch?v=4fWyzwo1xg0&ab_channel=SimonGarfunkelVEVO", "https://youtu.be/pB-5XG-DbAA?si=6KbkkYTIasxiu7gB", "https://youtu.be/k4V3Mo61fJM?si=iqPJN6SGt6DYawiT"],
+                "angry": ["https://youtu.be/CdhqVtpR2ts?si=ppEAZVi8xB2MDvia", "https://youtu.be/OBqw818mQ1E?si=jkF3crJLPb6ZjiDO"],
+                     "surprise": ["https://youtu.be/u5CVsCnxyXg?si=8jV3G07Dw8Og7HcN"]} #+more
 
     # Constructor method - needs 'self' as parameter
     def __init__(self):
@@ -45,6 +47,8 @@ class MusicLibrary:
             if (len(songList) > 0):
                 song = random.choice(songList)
                 print(song)
+                if(input("Wanna go listen?: ")):
+                    webbrowser.open_new_tab(song)
                 #if messagebox.askyesno("Play Song", "Do you want to play a song?"):
                     #webbrowser.open_new_tab(song)
 
