ace1dec60a061892d3b4771fd94714c1d905cec4
RimonaG. <cna8eg@virginia.edu>
2025-03-30 03:56:31 -0400

    added new image

--- src/main/EmptionDetection.py
+++ src/main/EmptionDetection.py
@@ -1,13 +1,18 @@
 #from xxsubtype import bench
 
+from MusicLibrary import MusicLibrary
+
+library = MusicLibrary()
+
 from deepface import DeepFace
 import random
 import webbrowser
 
-img = "Angry.jpg"
+img = "~/Desktop/folder/Angry.jpg"
 
 result = DeepFace.analyze(img_path = img , actions = ["emotion"])
 dominant_emotion = result[0]["dominant_emotion"]
 print("Detected emotion:", dominant_emotion)
 
 #facial analysis
+library.
