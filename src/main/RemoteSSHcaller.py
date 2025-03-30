8f3b2353a7c40c2716809f534b6060cff50f8d62
RimonaG. <cna8eg@virginia.edu>
2025-03-30 06:36:22 -0400

    Implemented connection to SSH server as backend brain

--- src/main/RemoteSSHcaller.py
+++ src/main/RemoteSSHcaller.py
@@ -13,9 +13,11 @@ def analyze_image(file_path):
     # Step 2: Run emotion detection remotely
     result = subprocess.run([
         "ssh", "cna8eg@portal.cs.virginia.edu",
-        f"ssh cna8eg@gpusrv01.cs.virginia.edu 'python3 /u/cna8eg/hoohacks/HooHacks2025/src/main/run_analysis.py {remote_path}'"
-        #f"ssh cna8eg@gpusrv01.cs.virginia.edu 'python3 /u/cna8eg/hoohacks/HooHacks2025/src/main/run_analysis.py {remote_path}'"
+        f"/sw/ubuntu2204/ebu082024/software/common/compiler/gcccore/13.2.0/python/3.11.5/bin/python3 /u/cna8eg/hoohacks/HooHacks2025/src/main/run_analysis.py {remote_path}"
+        #f"ssh cna8eg@gpusrv01.cs.virginia.edu 'python3 /u/cna8eg/hoohacks/HooHacks2025/src/main/SSHConnector.py {remote_path}'"
     ], capture_output=True, text=True)
+
+    print(result.stderr)
 
     # Step 3: Get the emotion result
     emotion = result.stdout.strip()
