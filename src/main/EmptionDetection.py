45c1657c4b64a8a05d9896792d6ef7884aca92ca
RimonaG. <cna8eg@virginia.edu>
2025-03-30 04:25:21 -0400

    added contains_mood to MusicLibrary.py + implemented the rest of logic in EmotionDetection.py

--- src/main/EmptionDetection.py
+++ src/main/EmptionDetection.py
@@ -7,12 +7,26 @@ library = MusicLibrary()
 from deepface import DeepFace
 import random
 import webbrowser
+global dominant_emotion
 
-img = "~/Desktop/folder/Angry.jpg"
-
-result = DeepFace.analyze(img_path = img , actions = ["emotion"])
-dominant_emotion = result[0]["dominant_emotion"]
-print("Detected emotion:", dominant_emotion)
+def deepFace(image_path):
+    img = "~/Desktop/folder/Angry.jpg"
+    result = DeepFace.analyze(img_path = img , actions = ["emotion"])
+    dominant_emotion = result[0]["dominant_emotion"]
+    print("Detected emotion:", dominant_emotion)
+    return dominant_emotion
 
 #facial analysis
-library.
+def get_song():
+    name = #function from gui
+    if(not(library.contains_mood(deepFace(name)))):
+        print("No emotion detected")
+        #tell user to add a recommendation or just find one randomly?
+
+    else:
+        songList = library.get(deepFace(name))
+        if(len(songList) > 0):
+            song = random.choice(songList)
+            if(input("Do you want song to play? :") == "yes"):
+                webbrowser.open_new_tab(song.url)
+
