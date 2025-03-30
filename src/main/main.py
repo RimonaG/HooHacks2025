05fa322e63009cb8a3857b04744ba2a8c2290986
RimonaG. <cna8eg@virginia.edu>
2025-03-29 23:45:40 -0400

    main

--- src/main/main.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-print("Available devices:", tf.config.list_physical_devices())
-print("GPU available:", tf.config.list_physical_devices('GPU'))
