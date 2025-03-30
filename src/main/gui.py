4544855e4ad9c69fd59ddefe6c594d49c8952b07
slurmsicle <nfrimp@outlook.com>
2025-03-30 04:26:22 -0400

    gui

--- src/main/gui.py
+++ src/main/gui.py
@@ -1,18 +1,48 @@
 import tkinter as tk
+from tkinter import filedialog, messagebox
+import os
 
-def say_hello():
-	label.config(text="Hello, Rimon!")
-
+# Create the main window
 root = tk.Tk()
-root.title("My First GUI")
+root.title("Timer Button Example")
+root.geometry("300x200")
 
-root.geometry("500x300")
+# Function to show the second button after 3 seconds
+def show_second_button():
+	second_button = tk.Button(root, text="Click Here for Your Songs!")
+	second_button.pack(pady=10)
+	label_2.pack_forget()
 
-label = tk.Label(root, text="Click the button!")
-label.pack()
+# Function to start the timer
+def on_first_button_click():
+	#Open file dilog to select an image
+	file_path = filedialog.askopenfilename(
+	title="Select an Image",
+	filetypes=[("JPEG files", "*.jpg *.jpeg"), ("All files", "*.*")]
+	)
 
-button = tk.Button(root, text="Click Me", command=say_hello)
-button.pack()
+	#Check if the user selected a file
+	if file_path:
+		if file_path.lower().endswith(('.jpg', '.jpeg')):
+			label.config(text="Waiting 3 seconds...")
+			first_button.pack_forget()
+			label_2.config(text="Analyzing Photo...")
+			label_2.pack(pady=10)
+			root.after(3000, show_second_button)  # 3000ms = 3 seconds
+			label.pack_forget()
+		else:
 
+
+# Initial label
+label = tk.Label(root, text="Click the button below")
+label.pack(pady=10)
+# Second label
+label_2 = tk.Label(root)
+
+
+# First button
+first_button = tk.Button(root, text="Click me", command=on_first_button_click)
+first_button.pack(pady=10)
+
+# Run the app
 root.mainloop()
-
